55fbca98455c4157898cfea18b21a2e7d1670b1f
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-10-01 21:05:55 +0600

    style: Update scrollbar styles in globals.css
    Update the scrollbar styles in the globals.css file to improve the appearance and user experience. The background color of the scrollbar track is changed to #1D293A, and the color of the scrollbar thumb is changed to #111A28. The border radius of the thumb is also adjusted to 5px for rounded corners.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Title from './Title';
+import Link from 'next/link'
 
 export default function About() {
     return (
@@ -10,18 +10,20 @@ export default function About() {
                         {`Hello! I’m Gaus Al Munir Tushar, a Software Engineer with deep expertise in the JavaScript stack and a strong focus on DevOps practices. I specialize in building scalable, high-performance web applications and optimizing development processes for maximum efficiency. With a degree in Computer Science Engineering from Northern University Bangladesh, I bring both technical skill and a passion for continuous improvement to every project. I’m eager to leverage my abilities to drive innovation and contribute to impactful solutions.`}
                     </p>
                     <div className="flex mt-6 justify-center sm:justify-start space-x-4">
-                        <button
+                        <Link
+                            href="#experience"
                             className="bg-primary text-white px-6 py-2 rounded"
-                            aria-label="Download Resume"
+                            aria-label="Experience"
                         >
                             Experience
-                        </button>
-                        <button
+                        </Link>
+                        <Link
+                            href="#skill"
                             className="border-2 border-primary text-white px-6 py-2 rounded"
                             aria-label="Learn more my skills"
                         >
                             My Skills
-                        </button>
+                        </Link>
                     </div>
                 </div>
                 <div className="md:w-1/3 mt-8 md:mt-0 md:ml-8">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,54 +1,88 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Menu from './Menu';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isSticky, setIsSticky] = useState(false);
 
     const toggleMenu = () => setIsOpen(!isOpen);
 
+    // Handle scroll to detect when header becomes sticky
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className="container flex items-center justify-between py-6 text-white mx-auto px-8">
-            <div>
-                <Image src="/favicon.svg" alt="Logo" width={45} height={45} />
-            </div>
-            <div className="flex items-center gap-5">
-                {/* Desktop Menu */}
-                <nav
-                    aria-label="Main Navigation"
-                    className="hidden md:flex items-center gap-5"
-                >
-                    <Menu />
-                </nav>
+        <header
+            className={`sticky top-0 z-50 px-12 py-4 transition-colors duration-500 ${
+                isSticky ? 'bg-tertiary' : 'bg-gray-900'
+            }`}
+        >
+            <div className="container flex items-center justify-between  text-white mx-auto ">
+                <div>
+                    <Image
+                        src="/favicon.svg"
+                        alt="Logo"
+                        width={45}
+                        height={45}
+                    />
+                </div>
+                <div className="flex items-center gap-5">
+                    {/* Desktop Menu */}
+                    <nav
+                        aria-label="Main Navigation"
+                        className="hidden md:flex items-center gap-5"
+                    >
+                        <Menu />
+                    </nav>
 
-                {/* Mobile Menu Button */}
-                <button
-                    className="md:hidden text-white bg-primary py-2 px-3 rounded hover:bg-green-500 transition-colors"
-                    onClick={toggleMenu}
-                    aria-label={isOpen ? "Close Menu" : "Open Menu"} // Screen reader friendly
-                    aria-expanded={isOpen} // Indicate if the menu is expanded
-                    aria-controls="mobile-menu" // Link button to the menu
-                >
-                     {isOpen ? <FaTimes size={24} aria-hidden="true" /> : <FaBars size={24} aria-hidden="true" />}
-                </button>
-            </div>
+                    {/* Mobile Menu Button */}
+                    <button
+                        className="md:hidden text-white bg-primary py-2 px-3 rounded hover:bg-green-500 transition-colors"
+                        onClick={toggleMenu}
+                        aria-label={isOpen ? 'Close Menu' : 'Open Menu'} // Screen reader friendly
+                        aria-expanded={isOpen} // Indicate if the menu is expanded
+                        aria-controls="mobile-menu" // Link button to the menu
+                    >
+                        {isOpen ? (
+                            <FaTimes size={24} aria-hidden="true" />
+                        ) : (
+                            <FaBars size={24} aria-hidden="true" />
+                        )}
+                    </button>
+                </div>
 
-            {/* Mobile Menu */}
-            <div
-                className={`fixed top-20 left-0 w-full h-full bg-gray-900 text-white md:hidden transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'} flex flex-col items-center pt-20`}
-                role="dialog" // Role for the mobile menu as a dialog
-                aria-modal="true" // Indicate that it is a modal
-                aria-labelledby="mobile-menu-title" // Link to a menu title for contextual screen readers
-            >
-                <Menu mobile={true} />
-                <button
-                    className="mt-6 border-2 border-green-500 text-white uppercase py-1.5 px-4 rounded hover:bg-green-500 transition-colors"
-                    onClick={toggleMenu}
+                {/* Mobile Menu */}
+                <div
+                    className={`fixed top-20 left-0 w-full h-full bg-gray-900 text-white md:hidden transition-transform duration-300 ${
+                        isOpen ? 'translate-x-0' : '-translate-x-full'
+                    } flex flex-col items-center pt-20`}
+                    role="dialog" // Role for the mobile menu as a dialog
+                    aria-modal="true" // Indicate that it is a modal
+                    aria-labelledby="mobile-menu-title" // Link to a menu title for contextual screen readers
                 >
-                    Resume
-                </button>
+                    <Menu mobile={true} />
+                    <button
+                        className="mt-6 border-2 border-green-500 text-white uppercase py-1.5 px-4 rounded hover:bg-green-500 transition-colors"
+                        onClick={toggleMenu}
+                    >
+                        Resume
+                    </button>
+                </div>
             </div>
         </header>
     );
